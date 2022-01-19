8a58c1d4835801aaf7d0a2d649d32ef2cf08cf09
oleniev_vova <oleniev9@gmail.com>
2022-01-19 18:06:49 +0200

    first commit

--- src/components/DownLoad.vue
+++ src/components/DownLoad.vue
@@ -18,28 +18,10 @@ export default {
       this.selectedImage = event.target.files[0]
     },
     ...mapActions(['upLoadImage', 'selectFile'])
-
   },
 }
 </script>
 
 <style scoped>
-.heart {
-  position: relative;
-  top: 70px;
-  height: 30px;
-  width: 30px
-}
 
-.catItems {
-  box-sizing: border-box;
-  flex-wrap: wrap;
-}
-
-.catPicture {
-  height: 300px;
-  width: 300px;
-  margin: 10px;
-  border: 1px solid gold;
-}
 </style>
